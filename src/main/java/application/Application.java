aa66d51543b4668ccf2153b13ad7368a7571cd5a
felipe <felipe@soundview.com.br>
2017-06-30 15:50:33 -0300

    Removing PersistenceContext, adding EntityScan.

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -2,6 +2,7 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @ServletComponentScan(value="security")
 @ComponentScan({"services", "controller", "persistence", "security"})
+@EntityScan({"model"})
 @EnableJpaRepositories(basePackages={"repository"})
 public class Application {
  
